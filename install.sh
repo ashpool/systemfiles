7d4c7d09745b2f85b7924d13961f6e9f9c9fc76a
Wouter de Bie <wouter.de.bie@deltaprojects.se>
2011-05-17 15:01:49 +0200

    Some stats in install script

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ dotfilepath="$PWD/dotfiles"
 now=$(date +%Y%m%d%H%M%S)
 mkdir -p backups
 
+skiped=0
+linked=0
+backedup=0
+dircreated=0
+
 files=$(find $dotfilepath -type f)
 for file in $files; do
   basefile=$(echo $file | sed s#$dotfilepath/##g)
@@ -12,8 +17,9 @@ for file in $files; do
 
   link=$(readlink ~/$basefile)
   if [ "$link" == "$file" ]; then
-    echo "   [NOLINK] ~/$basefile already linked to $file"
+    echo "   [NOLINK] ~/$basefile already linked correctly"
     echo -e
+    ((skipped++))
     continue
   fi
 
@@ -26,17 +32,20 @@ for file in $files; do
 
     echo "   [REMOVE] ~/$basefile"
     rm -f ~/$basefile
+    ((backedup++))
   fi
 
   if [ ! -d `dirname ~/$basefile` ]; then
     directory=$(dirname ~/$basefile)
     echo "   [CREATE DIR] $directory"
     mkdir -p $directory 
+    ((dircreated++))
   fi 
 
   echo "   [LINK] $file ==> ~/$basefile"
   ln -s $file ~/$basefile
   echo -e
+  ((linked++))
 done
 
 
@@ -56,4 +65,8 @@ if [ ! -f ~/.rvm/bin/rvm ]; then
   echo "== [RVM] installing"
   bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
   source ~/.bash_profile
+  echo -e 
 fi
+
+echo "$skipped skipped, $linked linked, $backedup backed up, $dircreated dirs created"
+
