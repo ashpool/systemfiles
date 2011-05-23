5cfd4c54e3c2f525357e2d408558131247a24ea1
Wouter de Bie <wouter@evenflow.se>
2011-05-23 08:23:18 +0200

    Changed stuff in the install script

--- install.sh
+++ install.sh
@@ -13,58 +13,58 @@ dircreated=0
 files=$(find $dotfilepath -type f)
 for file in $files; do
   basefile=$(echo $file | sed s#$dotfilepath/##g)
-  echo "== [DOTFILE] installing ~/$basefile"
+  homefile="$HOME/$basefile"
+  echo "== [DOTFILE] installing $homefile"
 
-  link=$(readlink ~/$basefile)
+  link=$(readlink $homefile)
   if [ "$link" == "$file" ]; then
-    echo "   [NOLINK] ~/$basefile already linked correctly"
+    echo "   [NOLINK] $homefile already linked correctly"
     echo -e
     ((skipped++))
     continue
   fi
 
-  if [ -f ~/$basefile ]; then
+  if [ -f $homefile ]; then
     backuppath="$PWD/backups/$basefile-$now"
     mkdir -p `dirname $backuppath`
     
-    echo "   [BACKUP] ~/$basefile to $backuppath"
-    cp ~/$basefile $backuppath
+    echo "   [BACKUP] $homefile to $backuppath"
+    cp $homefile $backuppath
 
-    echo "   [REMOVE] ~/$basefile"
-    rm -f ~/$basefile
+    echo "   [REMOVE] $homefile"
+    rm -f $homefile
     ((backedup++))
   fi
 
-  if [ ! -d `dirname ~/$basefile` ]; then
-    directory=$(dirname ~/$basefile)
+  if [ ! -d `dirname $homefile` ]; then
+    directory=$(dirname $homefile)
     echo "   [CREATE DIR] $directory"
     mkdir -p $directory 
     ((dircreated++))
   fi 
 
-  echo "   [LINK] $file ==> ~/$basefile"
-  ln -sf $file ~/$basefile
+  echo "   [LINK] $file ==> $homefile"
+  ln -sf $file $homefile
   echo -e
   ((linked++))
 done
 
 
-if [ -f ~/.gitlocal ]; then 
+if [ -f $HOME/.gitlocal ]; then 
   echo "== [GIT] using local configuration"
-  sed -i '' -e "/# GITLOCAL/r $HOME/.gitlocal" ~/.gitconfig
+  sed -i '' -e "/# GITLOCAL/r $HOME/.gitlocal" $HOME/.gitconfig
 else
   echo "== [GIT] using default configuration"
-  sed -i '' -e "/# GITLOCAL/r $HOME/.gitdefaults" ~/.gitconfig
+  sed -i '' -e "/# GITLOCAL/r $HOME/.gitdefaults" $HOME/.gitconfig
 fi
 
-sed -i -e 's/^# GITLOCAL$//g' ~/.gitconfig
+sed -i -e 's/^# GITLOCAL$//g' $HOME/.gitconfig
 
 echo -e
 
-if [ ! -f ~/.rvm/bin/rvm ]; then
+if [ ! -f $HOME/.rvm/bin/rvm ]; then
   echo "== [RVM] installing"
   bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
-  source ~/.bash_profile
   echo -e 
 fi
 
@@ -77,6 +77,10 @@ fi
 
 echo "== [VIM] Vundle update"
 vim +BundleInstall! +BundleClean +q
+echo -e 
 
+echo "== [BASH] Reloading"
+source $HOME/.bash_profile
+echo -e
 echo "$skipped skipped, $linked linked, $backedup backed up, $dircreated dirs created"
 
